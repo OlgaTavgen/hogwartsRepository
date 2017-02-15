9e01b791521dffd545fdf41bb4b7b2b0ef76d9f9
Olga Tavgen <Olga_Tavgen@EPBYMINW3635.minsk.epam.com>
2017-02-15 18:19:17 +0300

    commit for 1 module

--- hogwarts/hogwartsproject/target/hogwartsproject/js/hogwartsDashboard.js
+++ hogwarts/hogwartsproject/target/hogwartsproject/js/hogwartsDashboard.js
@@ -1,19 +1,72 @@
-
-//$('#li-element').click(function() {
-//	$.ajax({
-//		url: "localhost:8080/hogwartsproject/hogwarts/dashboard",
-//		data : {
-//			format: 'json'
-//		},
-//		success: function(data) {
-//			var $team = $('<h1>').text(data.team)
-//		},
-//		type: 'GET'
-//	})
-//})
-
-$(function() {
-    $('#li-element').click(function() {
-        alert(this.id); 
-    });
-})
+	var xhr = new XMLHttpRequest();
+	
+	var elements = document.getElementsByTagName('a');
+	
+	for (var i=0; i<elements.length; i++)	{				
+		
+ 		var text = elements[i].textContent;
+		
+		elements[i].addEventListener('click', function() {
+									
+			var name = 'Gryffindor';
+				
+			xhr.open('GET', 'dashboard.json?team='+text, false);
+			
+			xhr.send();
+			
+		 	if (xhr.status != 200)
+				{
+				alert(xhr.status + ':' + xhr.statusText);
+				}
+			else {
+				/* alert(JSON.parse(xhr.responseText)); */
+				
+				 var json = JSON.parse(xhr.responseText);
+				
+			 	 var table = document.createElement("table");
+				
+				 var tr = document.createElement('tr');
+				 
+				 var row1 = document.createElement('td');
+				 row1.innerHTML = "First Name";
+				 tr.appendChild(row1);
+				 
+				 var row2 = document.createElement('td');
+				 row2.innerHTML = "Last Name";
+				 tr.appendChild(row2);
+				 
+				 var row3 = document.createElement('td');
+				 row3.innerHTML = "Nickname";
+				 tr.appendChild(row3);
+				 
+				 var row4 = document.createElement('td');
+				 row4.innerHTML = "Primary Skill";
+				 tr.appendChild(row3);
+				 
+				 table.appendChild(tr);
+				 
+				 for(var developer in json.developers) {
+					 
+					 var developer = json[developer];				 
+					 var tr = document.createElement('tr');
+					 
+					 var td1 = document.createElement('td');
+					 td1.innerHTML = developer.firstName;
+					 
+					 var td2 = document.createElement('td');
+					 td2.innerHTML = developer.lastName;
+					 
+					 var td3 = document.createElement('td');
+					 td3.innerHTML = developer.nickname;
+					 
+					 var td4 = document.createElement('td');
+					 td4.innerHTML = developer.primarySkill;
+					 
+					 table.appendChild(tr);
+				 }
+				 
+				 document.body.appendChild(table);
+			}  			
+			
+		})			
+	}	
